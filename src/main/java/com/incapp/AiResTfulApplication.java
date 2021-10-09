c92f82f70936be855a79d4017885d56c3c6d82f3
RahulChauhan44 <92219110+RahulChauhan44@users.noreply.github.com>
2021-10-09 20:05:42 +0530

    Add files via upload

---
+++ src/main/java/com/incapp/AiResTfulApplication.java
@@ -0,0 +1,13 @@
+package com.incapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiResTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiResTfulApplication.class, args);
+	}
+
+}
